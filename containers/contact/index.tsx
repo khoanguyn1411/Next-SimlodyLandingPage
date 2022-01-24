935af413aad83a9474173091f46c6f22078562bc
Huynh Nhat Hao <haohuynh11011999@gmail.com>
2022-01-24 17:36:21 +0700

    update content contact

--- containers/contact/index.tsx
+++ containers/contact/index.tsx
@@ -4,7 +4,7 @@ export const ContactContainer = () => {
   return (
     <div className="flex items-center justify-center p-8">
       <div className="container">
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-3 gap-32">
           <div className="col-span-2 space-y-8">
             <h2 className="text-4xl font-semibold">Contact Us</h2>
 
@@ -37,19 +37,32 @@ export const ContactContainer = () => {
 
               <FormItem label="What can we help you" isRequired>
                 <textarea
-                placeholder="ex: I want to build new Saas product for my company..."
+                  placeholder="ex: I want to build new Saas product for my company..."
                   rows={3}
                   className="px-4 py-3 bg-gray-100 rounded-md outline-none w-full"
                 />
               </FormItem>
 
-              <Button text="Submit Aplication" type="primary" block/>
+              <Button text="Submit Aplication" type="primary" block />
             </form>
           </div>
 
-          <div className="col-span-1">
+          <div className="col-span-1 flex justify-center w-full">
+            <div className="flex flex-col space-y-8">
+              <h2 className="text-4xl font-semibold">Office</h2>
 
-            
+              <span className="text-base">
+                Vietnam <br /> Vietan building, No. 4E, Street 6, Ward An Phu,
+                District 2, Ho Chi Minh City
+              </span>
+
+
+              <div className="flex flex-col space-y-4">
+              <span className="text-base">For Quick Inquiries</span>
+
+                <span>84+ 1652237832</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
