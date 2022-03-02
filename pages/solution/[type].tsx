a3719ac882d53383cc9c369390fc786dd38c5ee4
Dai Nguyen <ncdai3651408@gmail.com>
2022-03-02 22:57:49 +0700

    Fixed: solution site cover

--- pages/solution/[type].tsx
+++ pages/solution/[type].tsx
@@ -1,54 +1,72 @@
 import { LayoutBase } from "@components"
 import { SolutionContainer } from "@containers"
+import { NextPage, NextPageContext } from "next";
 import { useRouter } from "next/router";
 import { useMemo } from "react";
 
-const SolutionPage: React.FC = () => {
+const getPageHead = (type: string): {
+  title: string
+  cover: string
+} => {
+  switch (type) {
+    case "product":
+      return {
+        title: "Product Concept | Solution",
+        cover: "/images/site-covers/product.jpg"
+      };
+
+    case "saas":
+      return {
+        title: "Software As A Service | Solution",
+        cover: "/images/site-covers/sass.jpg"
+      };
+
+    case "odoo":
+      return {
+        title: "Odoo | Solution",
+        cover: "/images/site-covers/odoo.jpg"
+      };
+
+    default:
+      return {
+        title: "Unknown",
+        cover: ""
+      };
+  }
+}
+
+type IProps = {
+  pageTitle: string
+  pageCoverImageRectangle: string;
+}
+
+const SolutionPage: NextPage<IProps> = ({ pageTitle, pageCoverImageRectangle }) => {
     const router = useRouter();
     const type = router.query?.type as string;
-
-    const pageHead: {
-        title: string
-        cover: string
-      } = useMemo(() => {
-        switch (type) {
-          case "product":
-            return {
-              title: "Product Concept | Solution",
-              cover: "/images/site-covers/product.jpg"
-            };
-
-          case "saas":
-            return {
-              title: "Software As A Service | Solution",
-              cover: "/images/site-covers/sass.jpg"
-            };
-
-          case "odoo":
-            return {
-              title: "Odoo | Solution",
-              cover: "/images/site-covers/odoo.jpg"
-            };
-
-          default:
-            return {
-              title: "Unknown",
-              cover: ""
-            };
-        }
-      }, [type]);
 
     return(
         <LayoutBase
             pageKey="Solution"
             head={{
-                pageTitle: pageHead.title,
-                pageCoverImageRectangle: pageHead.cover
+                pageTitle,
+                pageCoverImageRectangle
             }}
         >
             <SolutionContainer/>
         </LayoutBase>
     )
 }
+
+SolutionPage.getInitialProps = async (
+  context: NextPageContext
+) => {
+  const type = context?.query?.type as string;
+  const pageHead = getPageHead(type)
+
+  return {
+    pageTitle: pageHead.title,
+    pageCoverImageRectangle: pageHead.cover
+  };
+};
 
 export default SolutionPage
