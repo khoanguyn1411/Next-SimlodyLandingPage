0cd01b4c317c6945d733f1c1d39a234b5e0465f0
khoanguyn1411 <khoaah1411@gmail.com>
2022-11-24 17:05:07 +0700

    Improve introduce section style

--- containers/home/components/introduce/Introduce.tsx
+++ containers/home/components/introduce/Introduce.tsx
@@ -1,46 +1,53 @@
+import { CurvesArrowIcon } from "@components";
 import { Button } from "@components/elements";
 import Image from "next/image";
 export const IntroduceSection: React.FC = () => {
   return (
     <section
-      className="py-12 opacity-75"
+      className="opacity-90 h-[700px] flex items-center"
       style={{
         background:
           "radial-gradient(98.54% 152.74% at 98.54% 83.59%, rgba(253, 208, 211, 0.15) 0%, rgba(208, 234, 255, 0.15) 100%)",
       }}
     >
       <div className="container">
-        <div className="grid grid-cols-2 gap-4">
-          <div>
-            <div className="flex flex-col">
-              <h2 className="text-4xl font-semibold text-gray-800">
-                Công cụ{" "}
-                <span className="text-primary-800">quản trị tổ chức</span> hiện
-                đại và hiệu quả
-              </h2>
+        <div className="grid grid-cols-2">
+          <div className="flex flex-col gap-9 h-full justify-center">
+            <h2 className="text-5xl leading-tight font-medium text-gray-800 relative">
+              Công cụ <span className="text-primary-800">quản trị tổ chức</span>{" "}
+              hiện đại và hiệu quả
+              <span
+                data-aos="fade-left"
+                data-aos-delay="200"
+                data-aos-duration="500"
+                className="text-primary-800 absolute -right-16 top-20"
+              >
+                <CurvesArrowIcon size={80} />
+              </span>
+            </h2>
+            <h3 className="text-xl text-gray-600">
+              Gia tăng sức khoẻ tổ chức, nâng cao hiệu suất làm việc, và tập
+              trung dữ liệu chỉ trong 1 nền tảng.
+            </h3>
 
-              <h3 className="text-xl text-gray-400 mt-8">
-                Gia tăng sức khoẻ tổ chức, nâng cao hiệu suất làm việc, và tập
-                trung dữ liệu chỉ trong 1 nền tảng.
-              </h3>
-
-              <div className="grid grid-cols-2 gap-4 mt-8">
-                <Button text="Trải nghiệm ngay" type="primary" />
-                <Button text="Liên hệ" />
-              </div>
+            <div className="flex gap-7">
+              <Button text="Trải nghiệm ngay" className="w-44" type="primary" />
+              <Button text="Liên hệ" className="w-44" />
             </div>
           </div>
-          <div>
+          <div className="flex justify-start">
             <div
               data-aos="fade-left"
               data-aos-delay="200"
               data-aos-duration="500"
-              className="p-12 w-full h-96"
+              className="p-12 w-full h-96 relative"
             >
               <Image
                 src={"/images/home/introduce.png"}
-                alt="introduce"
+                alt="Introduction"
                 layout="fill"
+                objectFit="contain"
+                className="w-full"
               />
             </div>
           </div>
