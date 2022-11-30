db5a9e1eb822d885ba0680b3a9d30883b61755f5
khoanguyn1411 <khoaah1411@gmail.com>
2022-11-30 11:55:46 +0700

    Add form email validation

--- containers/home/components/box-email/BoxEmail.tsx
+++ containers/home/components/box-email/BoxEmail.tsx
@@ -1,16 +1,40 @@
 import { MailIcon, SectionContainer } from "@components";
 import { Button, Input } from "@components/elements";
 import { BackgroundHeart } from "@components/icons/BackgroundHeart";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 const GOOGLE_API_URL =
   "https://script.google.com/macros/s/AKfycbyccwaPOlK3RGn-Y6x15EJvLT-euGc138rybxqqIaAu68Yx46AUkSsFtaalTWaeW7Ah/exec";
 
+const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const ERROR_MESSAGES = {
+  emptyField: "Vui lòng nhập trường này.",
+  notEmail: "Trường này phải là email.",
+};
+
 export const BoxEmail: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>(
+    ERROR_MESSAGES.emptyField
+  );
+  const [isClickedSubmit, setIsClickedSubmit] = useState<boolean>(false);
+  const [isShowError, setIsShowError] = useState<boolean>(false);
+
   const handleSubmitEmail = async () => {
+    setIsClickedSubmit(true);
+    if (email === "") {
+      setErrorMessage(ERROR_MESSAGES.emptyField);
+      setIsShowError(true);
+      return;
+    }
+    if (EMAIL_REGEX.test(email) === false) {
+      setIsShowError(true);
+      setErrorMessage(ERROR_MESSAGES.notEmail);
+      return;
+    }
     const formData = new FormData();
     formData.append("email", email);
     setIsLoading(true);
@@ -25,6 +49,17 @@ export const BoxEmail: React.FC = () => {
     }
     setIsLoading(false);
   };
+  const shouldShowError = isShowError && isClickedSubmit;
+
+  useEffect(() => {
+    if (email === "") {
+      setErrorMessage(ERROR_MESSAGES.emptyField);
+      setIsShowError(true);
+      return;
+    }
+    setIsShowError(false);
+  }, [email]);
+
   return (
     <SectionContainer
       className="py-40 relative"
@@ -37,15 +72,20 @@ export const BoxEmail: React.FC = () => {
           </h2>
         </div>
 
-        <div className="flex items-center space-x-4 relative">
-          <Input
-            bordered={true}
-            className="pr-[155px]"
-            placeholder="Vui lòng điền Email của bạn tại đây"
-            prefix={<MailIcon />}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+        <div className="flex relative">
+          <div className="w-full flex flex-col gap-2">
+            <Input
+              bordered
+              className="pr-[155px]"
+              placeholder="Vui lòng điền Email của bạn tại đây"
+              prefix={<MailIcon />}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {shouldShowError && (
+              <span className="text-red-400">{errorMessage}</span>
+            )}
+          </div>
           <div className="min-w-max absolute right-0">
             <Button
               loading={isLoading}
